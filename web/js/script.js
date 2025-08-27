f1be4593eb2d28802fd0ec6bbf2cf314678893d4
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-08-27 15:51:36 +0530

    sng

--- web/js/script.js
+++ web/js/script.js
@@ -1,9 +1,15 @@
 let topnav = document.querySelector(".topnav")
 window.addEventListener("scroll",()=>{
-    if(window.scrollY > 50){
+    if(window.scrollY > 30){
         topnav.classList.add("scrolled")
     }
     else{
          topnav.classList.remove("scrolled")
     }
+})
+document.querySelector(".menuclose").addEventListener("click",()=>{
+document.querySelector(".menu").style.right="-350px"
+})
+document.querySelector(".menuopen").addEventListener("click",()=>{
+    document.querySelector(".menu").style.right="0"
 })

6b1c73d568ee0fe0fb9240c181e824732b323ec0
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-09-01 18:18:08 +0530

    cng

--- web/js/script.js
+++ web/js/script.js
@@ -21,9 +21,28 @@ dropList.addEventListener("click", () => {
   subList.classList.toggle("active");
 });
 
-document.querySelector(".bi-list-columns-reverse").addEventListener("click",()=>{
-document.querySelector("#accordionExample").classList.toggle("filteropen")
-})
+// document.querySelector(".bi-list-columns-reverse").addEventListener("click",()=>{
+// document.querySelector("#accordionExample").classList.toggle("filteropen")
+// })
 // document.querySelector(".menuopen").addEventListener("click",()=>{
 //     document.querySelector(".menu").style.right="0"
 // })
+
+
+
+let checkbox = document.getElementById("bulkorder")
+let wholesalepack = document.getElementById("wholesalepack")
+let retailpack = document.getElementById("retailpack")
+checkbox.addEventListener("change",function(){
+    if(this.checked){
+wholesalepack.style.display="block"
+retailpack.style.display="none"
+console.log("checked",retailpack);
+
+    }
+    else{
+        wholesalepack.style.display="none"
+        retailpack.style.display="block"
+        console.log("unchecked",retailpack);
+    }
+})
